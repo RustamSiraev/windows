2e7ca48cc9e51ca30c4f7849aa029848f39178e5
Rustam Siraev <sirius0983@mail.ru>
2021-04-20 22:28:06 +0500

    20-01-21

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,18 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener('DOMContentLoaded', () => {
-    let modalState = {};
+    let modalState = {},
+        deadline = '2021-04-15';
+
     changeModalState(modalState);
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms(modalState);
+    timer('.container1' ,deadline);
 })
 
